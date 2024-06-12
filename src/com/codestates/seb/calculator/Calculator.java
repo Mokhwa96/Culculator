13b12448b77565ca5e8930e6b8f4ae07cdccdd63
Mokhwa96 <mokhwa96@gmail.com>
2024-06-12 11:33:34 +0900

    Auto

--- src/com/codestates/seb/calculator/Calculator.java
+++ src/com/codestates/seb/calculator/Calculator.java
@@ -1,11 +1,17 @@
-package com.codestates.seb.calculator;
+package src.com.codestates.seb.calculator;
 import java.util.Scanner;
 
 public class Calculator {
   public static void main(String[] args) {
     System.out.println("===Java Calculator===");
+    String
 
     Scanner input = new Scanner(System.in);
+    String inputValue = input.nextLine();
+    System.out.println(inputValue);
+    for (int i = 0 ; i< inputValue.length(); i++) {
+        if (inputValue[i] in ["+"])
+    }
         /*
             요구 사항에 따라 간단한 계산기를 만들어주세요.
             1. 사용자의 입력으로 첫 번째 숫자, 연산자, 두 번째 숫자를 받아야 합니다.
@@ -13,6 +19,6 @@ public class Calculator {
             3. 소수점 연산을 수행할 수 있어야 합니다.
             4. 연산 결과를 콘솔에 출력합니다.
         */
-    // TODO...
+    //
   }
 }
